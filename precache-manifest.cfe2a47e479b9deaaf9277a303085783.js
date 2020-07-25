d5f1db469e5d5618b900fe69e7e7fbfcfe6e726a
Mykhailo <mykhaylo1809@gmail.com>
2020-07-25 15:46:23 +0300

    Updates

---
+++ precache-manifest.cfe2a47e479b9deaaf9277a303085783.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6a9f773733218cfd13d6deb86e4a7a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3b3de510422f2afcbb84",
+    "url": "/todolist/static/css/main.3f730ee9.chunk.css"
+  },
+  {
+    "revision": "fb518bee6d2e853d69ba",
+    "url": "/todolist/static/js/2.61003ef6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.61003ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3de510422f2afcbb84",
+    "url": "/todolist/static/js/main.089979b8.chunk.js"
+  },
+  {
+    "revision": "2f0d671c9bf37be0d800",
+    "url": "/todolist/static/js/runtime-main.ea8f9beb.js"
+  },
+  {
+    "revision": "b7a04bd296fdab589b2c62b338df89cc",
+    "url": "/todolist/static/media/13457.b7a04bd2.jpg"
+  },
+  {
+    "revision": "3a98759da3392f42298d7d4538298e33",
+    "url": "/todolist/static/media/16876.3a98759d.jpg"
+  },
+  {
+    "revision": "5e1425f728e663f343acd7eeb12787da",
+    "url": "/todolist/static/media/instruction.5e1425f7.svg"
+  },
+  {
+    "revision": "7ba1a0c0b8fa4f89583d4d8d728437b5",
+    "url": "/todolist/static/media/key.7ba1a0c0.svg"
+  },
+  {
+    "revision": "be40430c8f1ca1108dfac6fd309727c9",
+    "url": "/todolist/static/media/logout.be40430c.svg"
+  },
+  {
+    "revision": "d6ad5ae16303204b37c3d95fd2598c1e",
+    "url": "/todolist/static/media/menu.d6ad5ae1.svg"
+  },
+  {
+    "revision": "ac64af35fc93b6e9600770a52d15639e",
+    "url": "/todolist/static/media/play.ac64af35.svg"
+  },
+  {
+    "revision": "3feaa72e8339936cb23cfe91dc1abb6c",
+    "url": "/todolist/static/media/settings.3feaa72e.svg"
+  },
+  {
+    "revision": "b56228ae7aed5a6dd088b37b8a3323d6",
+    "url": "/todolist/static/media/spinner.b56228ae.svg"
+  }
+]);
